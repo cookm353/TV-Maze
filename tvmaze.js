16d442a59aeb96340b7cffd0141ffd71c0750481
Matthew Cook <cookm353@gmail.com>
2022-04-21 13:06:51 -0400

    Correcting IDs

--- tvmaze.js
+++ tvmaze.js
@@ -12,27 +12,21 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
-  // ADD: Remove placeholder & make request to TVMaze search shows API.
+async function getShowsByTerm(term) {
+  const url = `https://api.tvmaze.com/search/shows?q=${term}`;
+  const resp = await axios.get(url);
+  const shows = [];
+  for (let show of resp.data) {
+    shows.push({
+      id: show.show.id,
+      name: show.show.name,
+      show: show.show.summary,
+      image: show.show.image.medium
+    })
+  }
+  console.log(shows)
 
-  return [
-    {
-      id: 1767,
-      name: "The Bletchley Circle",
-      summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
-           women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
-           and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
-           quickly realises she can only begin to crack the murders and bring
-           the culprit to justice with her former friends.</p>`,
-      image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
-    }
-  ]
+  return shows
 }
 
 
@@ -77,6 +71,7 @@ async function searchForShowAndDisplay() {
 }
 
 $searchForm.on("submit", async function (evt) {
+  // evt.preventDefault();
   evt.preventDefault();
   await searchForShowAndDisplay();
 });
